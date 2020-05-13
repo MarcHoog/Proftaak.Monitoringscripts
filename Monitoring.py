cd17addee5895ff437fcdc300858ee3b5956790d
Liezaxie <61009498+Liezaxie@users.noreply.github.com>
2020-05-13 11:46:46 +0200

    Update Monitoring.py
    GUI startup

--- Monitoring.py
+++ Monitoring.py
@@ -5,14 +5,13 @@ import os
 root = tk.Tk()
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 canvas.pack()
+
+frame = tk.Frame(root, bg="White")
+frame.place(relwidth=0.8,relheight=0.8,relx=0.1, rely=0.1)
+
+Start = tk.Button(root, text="Start", padx=10, pady=5, fg="White", bg="#263D42")
+Start.pack()
+
 root.mainloop()
 
-def test(text):
-    print(text)
 
-#zet je naam er even bij in de list en push dan de file Danku!
-
-list = ['marc', 'kek', 'merijn', 'Lisa']
-
-for words in list:
-    test(words)
